2177245521c78ec50ff7d9f3a397ba95c2d6e6d3
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-12-31 15:41:03 +0100

    improved colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen h-[892px] lg:h-auto flex-col items-center justify-between p-12 lg:p-24 overflow-x-hidden">
       <div className="flex flex-col gap-6">
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-          <h1 className="text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-500 dark:from-sky-900 dark:to-blue-900 sm:text-8xl sm:bg-gradient-to-br sm:to-blue-500 sm:dark:from-transparent sm:dark:to-blue-900 lg:text-6xl">
+          <h1 className="text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-blue-500 dark:from-transparent dark:via-indigo-900 dark:to-blue-900 sm:text-8xl sm:bg-gradient-to-br sm:to-blue-500 lg:text-6xl">
             Nicolas Marchand
           </h1>
         </div>
@@ -78,7 +78,7 @@ export default function Home() {
           </p>
         </a>
       </div>
-      <span>© 2024 Nicolas Marchand</span>
+      <span className="opacity-80 text-sm">© 2024 Nicolas Marchand</span>
     </main>
   );
 }
